318808e9d2bed4c27df0eeee20cbd38d2f59af82
Florian Gehin <florian.gehin@epitech.eu>
2025-06-24 11:57:33 +0200

    build(core): add poetry, eplace django by fastAPI

---
+++ src/wall_e/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from services.wall_eV2 import scrap_watch_list
+
+app = FastAPI()
+
+@app.get("/")
+async def hello_world():
+    return {"message": "Hello world"}
+
+# @app.get("/api/v1/{user}/watchlist")
+# async def scrap_user(user: str):
+    # TODO: Ajouté la logique
