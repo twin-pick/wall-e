2ceb18872c9e9b65326675cda0ab57ada1ae49fd
Florian Gehin <florian.gehin@epitech.eu>
2025-06-30 11:47:28 +0200

    feat(main): add a route for the first version of te scrapper (movie name only)

--- src/wall_e/main.py
+++ src/wall_e/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from services.wall_eV2 import scrap_watch_list
+from services.wall_eV2 import scrap_watch_list as scrap_v2
+from services.wall_e import scrap_watch_list as scrap
 
 app = FastAPI()
 
@@ -10,5 +11,11 @@ async def hello_world():
 @app.get("/api/v1/{user}/watchlist")
 async def scrap_user(user: str):
     print(user)
-    list = scrap_watch_list(user)
+    list = scrap(user)
+    return list
+
+@app.get("/api/v2/{user}/watchlist")
+async def scrap_user(user: str):
+    print(user)
+    list = scrap_v2(user)
     return list
